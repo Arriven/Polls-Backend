c0e0fd12d8d8a78234cdccdd4856d521a81a3ef8
Arriven <bogdannuch@gmail.com>
2018-03-13 22:47:04 +0200

    made simple DAO objects for polls, questions and answers. added simple tests

--- src/main/java/polls/Poll.java
+++ src/main/java/polls/Poll.java
@@ -2,9 +2,18 @@ package polls;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Poll {
+	private static AtomicLong counter = new AtomicLong();
 	private List<Question> questions = new ArrayList<Question>();
+	private final long id;
+	private final String name;
+
+	public Poll(String name) {
+		this.id = counter.incrementAndGet();
+		this.name = name;
+	}
 
 	public void addQuestion(Question q) {
 		questions.add(q);
@@ -12,5 +21,9 @@ public class Poll {
 
 	public List<Question> getQuestions() {
 		return questions;
+	}
+
+	public long getId() {
+		return id;
 	}
 }
