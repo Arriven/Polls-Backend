17f40e9426e1fa7fbd35f599585935faaf930029
Arriven <bogdannuch@gmail.com>
2018-04-29 15:40:46 +0300

    simplified web security a bit

--- src/main/java/polls/WebSecurityConfig.java
+++ src/main/java/polls/WebSecurityConfig.java
@@ -17,8 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.antMatchers("/getPoll").permitAll()
-				.antMatchers("/getPollsList").permitAll()
+				.antMatchers("/getPoll", "/getPollsList").permitAll()
 				.anyRequest().authenticated()
 				.and()
 			.formLogin()
