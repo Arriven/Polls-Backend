008e530d508638bcbefb31d6efc3c3eac1ef98b5
Arriven <bogdannuch@gmail.com>
2018-05-19 16:46:44 +0300

    remove authorization requirement for completing the poll

--- src/main/java/polls/WebSecurityConfig.java
+++ src/main/java/polls/WebSecurityConfig.java
@@ -17,7 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.antMatchers("/getPoll", "/getPollsList").permitAll()
+				.antMatchers("/getPoll", "/getPollsList", "/result").permitAll()
 				.anyRequest().authenticated()
 			.and()
 				.httpBasic()
