f229ce1290b1ba10e0c0c9933b44b669c493a2f3
Bohdan Ivashko <Bogdannuch@gmail.com>
2018-05-20 18:22:56 +0300

    trying to setup mongo on heroku

---
+++ src/main/java/polls/MongoConfig.java
@@ -0,0 +1,19 @@
+package polls;
+
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableMongoRepositories()
+public class MongoConfig extends AbstractMongoConfiguration {
+    @Bean(name = "mongoClient")
+    public MongoClient mongo() throws UnknownHostException {
+        String connection = "mongodb://heroku_temp_test_user:temp_password@ds119820.mlab.com:19820/heroku_90t758ft";
+        return new MongoClient(new MongoClientURI(connection));
+    }
+}
